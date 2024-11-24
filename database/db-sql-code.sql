2c22b8df2caad339df723ff44e1c3a470225939c
Joan Cerepi <cyberstriker27@proton.me>
2024-11-23 22:37:56 -0700

    completed week 03 learning activities

--- database/db-sql-code.sql
+++ database/db-sql-code.sql
@@ -2,7 +2,7 @@ CREATE TYPE public.account_type AS ENUM
     ('Client', 'Employee', 'Admin');
 
 ALTER TYPE public.account_type
-    OWNER TO cse340;
+    OWNER TO cse340_database_bpbp_user;
 
 -- Table structure for table `classification`
 CREATE TABLE public.classification (
